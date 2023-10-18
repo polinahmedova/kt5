2fc4e8ea00c0998083154d25caa9cee79ce85cfb
polinahmedova <polinahmedova510@gmail.com>
2023-10-18 20:56:02 +0500

    finaly

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
                   textDirection: TextDirection.ltr,
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.black,
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
